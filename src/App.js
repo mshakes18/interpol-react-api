283ea6e84f8c230339e7712edc96f36a4aa06b55
Marcus S <marcusshakes@gmail.com>
2023-03-12 20:00:39 +0000

    changing request

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import "./App.css";
 import Axios from "axios";
-import React, { useState } from "react";
 
 function App() {
-  const [wantedNames, setWantedNames] = useState([]);
   const getWantedNames = () => {
     Axios.get(
       "https://ws-public.interpol.int/notices/v1/red?nationality=DE",
     ).then((response) => {
-      setWantedNames(response.data._embedded.notices);
+      console.log(response.data._embedded.notices[0].forename);
     });
   };
   return (
     <div className="App">
       <h1>Hello world</h1>
+      <p>{getWantedNames()}</p>
     </div>
   );
 }
