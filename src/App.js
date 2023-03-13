f9b83810897377f038ab24acff62c10bd5b814bd
Marcus S <marcusshakes@gmail.com>
2023-03-13 22:43:41 +0000

    api attempt

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import "./App.css";
 import Axios from "axios";
+import { useEffect, useState } from "react";
 
 function App() {
-  const getWantedNames = () => {
+  const [wantedNames, setWantedNames] = useState([]);
+  useEffect(() => {
     Axios.get(
       "https://ws-public.interpol.int/notices/v1/red?nationality=DE",
     ).then((response) => {
       console.log(response.data._embedded.notices[0].forename);
     });
-  };
+  }, []);
+
   return (
     <div className="App">
       <h1>Hello world</h1>
-      <p>{getWantedNames()}</p>
     </div>
   );
 }
