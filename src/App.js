b3cb42e90d975e3f412d47e1722986254ba8abb4
Marcus S <marcusshakes@gmail.com>
2023-03-28 21:52:03 +0100

    adding

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import Axios from "axios";
 import { useEffect, useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   const [wantedNames, setWantedNames] = useState([]);
@@ -8,13 +9,29 @@ function App() {
     Axios.get(
       "https://ws-public.interpol.int/notices/v1/red?nationality=DE",
     ).then((response) => {
-      console.log(response.data._embedded.notices[0].forename);
+      // console.log(response.data._embedded.notices);
+      setWantedNames([...response.data._embedded.notices]);
     });
   }, []);
 
+  const wantedDetails = wantedNames.map((person) => {
+    return (
+      <div>
+        <h3>
+          <a href={`/${person.entity_id}`}>
+            {person.forename + " " + person.name}
+          </a>
+        </h3>
+        <p>Date of Birth: {person.date_of_birth}</p>
+        <hr />
+      </div>
+    );
+  });
+
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <h1>Interpol Most Wanted: Red Notice</h1>
+      {wantedDetails}
     </div>
   );
 }
